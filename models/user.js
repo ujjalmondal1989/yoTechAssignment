bdf21f3dc973196a919980238485307d2fbf68cd
Ujjal Mondal <ujjal.mondal@goodworklabs.com>
2018-10-01 14:20:05 +0530

    file upload implemented

--- models/user.js
+++ models/user.js
@@ -17,7 +17,8 @@ var UserSchema = new Schema({
 
 	profile: {
 		name: {type: String, default: ''},
-		picture: {type: String, default: ''}
+		picture: {type: String, default: ''},
+		designation:{type: String, default: ''}
 	},
 
 	address: String
--- routes/main.js
+++ routes/main.js
@@ -1,11 +1,23 @@
 // external imports
 var router = require('express').Router();
 var passport = require('passport');
+var multer = require('multer');
 
 // custom imports
 var User = require('../models/user');
 var passportConf = require('../config/passport');
 
+//multer setup
+var storage = multer.diskStorage({
+    destination: function(req,res,callback){
+        callback(null,'./public/uploads');
+    },
+    filename:function(req,filename,callback){
+        callback(null,Date.now()+filename.originalname);
+    }
+});
+
+// landing page for login
 router.get('/', function (req, res) {
 	if (req.isAuthenticated()) {
       return res.redirect("/profile");
@@ -39,9 +51,45 @@ router.get('/auth/google/callback', passport.authenticate('google', {
     failureRedirect: '/'
 }));
 
-
+//profile form route
 router.get('/profile',passportConf.isAuthenticated, function (req, res) {
-	res.render('accounts/dashboard',{user:req.user});
+	res.render('accounts/dashboard',{user:req.user,message:req.flash('updated'),error:req.flash('error')});
+});
+
+
+// post profile route
+router.post('/postProfile',passportConf.isAuthenticated, function (req, res) {
+    User.findOne({ email: req.user.email }, function(err, existingUser) {
+        if(err){
+            req.flash('error', 'user not found');
+            return  res.redirect('/profile');
+        }
+        //form file setup
+        const fileUpload = multer({storage:storage}).single('profilePic');
+        fileUpload(req,res,function(err){
+            console.log(req.file);
+            if(err){
+                req.flash('error', 'Error uploading file'); 
+                res.redirect('/profile');
+            }
+            if(req.file != undefined && req.file!= ''){
+                const newFileName = req.file.filename;
+                existingUser.profile.picture = newFileName;
+            }
+            
+            existingUser.profile.name = req.body.username;            
+            existingUser.address = req.body.useraddress;   
+            existingUser.profile.designation = req.body.userdesignation;   
+            existingUser.save(function(err) {
+                if (err){
+                    req.flash('error', 'Some error occured');
+                    return res.redirect('/profile');
+                }
+                req.flash('updated', 'Successfully updated the details');
+                return res.redirect('/profile');
+            });     
+        });
+    });
 });
 
 router.get('/logout', function (req, res) {
